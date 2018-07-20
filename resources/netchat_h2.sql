0fde5bc7acd211a7bdbcf9dd5d83fd57c68d9fdf
ll <1005252998@qq.com>
2018-07-20 19:32:34 +0800

    添加了一些包名

---
+++ resources/netchat_h2.sql
@@ -0,0 +1,6 @@
+-- 建表user --
+CREATE TABLE user(
+u_account varchar(50) PRIMARY KEY,
+u_name varchar(50) not null,
+u_password varchar(50) not null,
+);
---
+++ src/main/java/netchat/netchatserver/RequestMapping.java
@@ -0,0 +1,7 @@
+package netchat.netchatserver;
+
+public class RequestMapping {
+	public static void mapping(String cmd, String data) {
+		
+	}
+}
