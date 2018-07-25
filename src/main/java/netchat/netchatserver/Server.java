f4924316940bfb057974d0cc11f902eda26fe716
liang <1005252998@qq.com>
2018-07-25 18:54:37 +0800

    修改了请求应答服务为线程启动

--- src/main/java/netchat/netchatserver/Server.java
+++ src/main/java/netchat/netchatserver/Server.java
@@ -6,36 +6,37 @@ import org.zeromq.ZMQ;
  * Hello world! 这是服务端的测试类
  */
 public class Server {
-//	ThreadLocal<>
-	
-	public static void main(String[] args) throws InterruptedException {
-		ZMQ.Context context = ZMQ.context(1);
 
-		// Socket to talk to clients
-		ZMQ.Socket responder = context.socket(ZMQ.REP);
-		responder.bind("tcp://*:5555");
-
-		while (!Thread.currentThread().isInterrupted()) {
-			// Wait for next request from the client
-			byte[] request = responder.recv(0);
-			System.out.println("Received " + new String(request));
-
-			// Do some 'work'
-			Thread.sleep(1000);
-
-			// Send reply back to client
-			String reply = "World";
-			responder.send(reply.getBytes(), 0);
-		}
-		responder.close();
-		context.term();
-	}
-	
-	public class MyThread implements Runnable{
+	//请求应答模式的服务线程
+	public static class rrThread implements Runnable {
 		@Override
 		public void run() {
-			// TODO Auto-generated method stub
-			
+			ZMQ.Context context = ZMQ.context(1);
+
+			// Socket to talk to clients
+			ZMQ.Socket responder = null;
+			responder = context.socket(ZMQ.REP);
+			responder.bind("tcp://*:5555");
+
+			System.out.println("服务器启动");
+			while (!Thread.currentThread().isInterrupted()) {
+				// 等待信息
+				byte[] request = responder.recv(0);
+				System.out.println("接受信息： " + new String(request));
+
+				// 工作
+				System.out.println("正在处理信息：" + new String(request));
+
+				// 发送回复信息
+				String reply = "已接收： " + new String(request);
+				responder.send(reply.getBytes(), 0);
+			}
+			responder.close();
+			context.term();
 		}
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		new Thread(new rrThread()).start();
 	}
 }
